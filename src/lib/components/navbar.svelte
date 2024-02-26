a527387ddfde215292193e77eb8ec981991db136
Jose Acosta <jdap.dmc@gmail.com>
2024-02-26 09:44:33 -0400

    fist commit

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,10 @@
+<header class="w-full h-1/4 fixed top-0">
+	<nav class="grid grid-cols-4">
+		<div class="w-3/4 h-full">
+			<img src="logo.svg" alt="hola" class="w-full h-32" />
+		</div>
+		<button>hola</button>
+		<button>hola</button>
+		<button>hola</button>
+	</nav>
+</header>
